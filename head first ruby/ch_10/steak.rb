0bb78679e82950d80cfcca35103c84231d42c444
Nguyen Hoang <hoang_nv@septeni-technology.jp>
2018-07-02 13:55:00 +0700

    head first ruby: complete chapter 9 and chapter 10

---
+++ head first ruby/ch_10/steak.rb
@@ -0,0 +1,38 @@
+class Steak
+    include Comparable
+    GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
+
+    attr_accessor :grade
+    def <=>(other)
+        if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+            return -1
+        elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+            return 0
+        else
+            return 1
+        end
+    end
+end
+
+first_steak = Steak.new
+first_steak.grade = "Prime"
+second_steak = Steak.new
+second_steak.grade = "Choice"
+
+puts first_steak <=> second_steak
+puts second_steak <=> first_steak
+
+prime = Steak.new
+prime.grade = "Prime"
+choice = Steak.new
+choice.grade = "Choice"
+select = Steak.new
+select.grade = "Select"
+
+puts "prime > choice: #{prime > choice}"
+puts "prime < select: #{prime < select}"
+puts "select == select: #{select == select}"
+puts "select <= select: #{select <= select}"
+puts "select >= choice: #{select >= choice}"
+print "choice.between?(select, prime): "
+puts choice.between?(select, prime)
